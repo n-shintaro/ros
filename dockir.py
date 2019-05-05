b0ed2bcff98da8e0616fb13902c314f7bf152863
n-shintaro <nakaoka.shintaro.infratop@gmail.com>
2019-05-05 21:05:45 +0900

    odom[

--- dockir.py
+++ dockir.py
@@ -5,12 +5,14 @@ from tf2_msgs.msg import TFMessage
 from kobuki_msgs.msg import DockInfraRed
 from kobuki_msgs.msg import WheelDropEvent
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Quaternion
+from nav_msgs.msg import Odometry
 class Kobuki():
 	def __init__(self):
                 self.sub_wheel = rospy.Subscriber('/mobile_base/events/wheel_drop', WheelDropEvent,self.stop,queue_size=1)
 		self.sub_dock_ir=rospy.Subscriber('/mobile_base/sensors/dock_ir',DockInfraRed,self.feel,queue_size=1)
 		
-		self.sub_tf=rospy.Subscriber('/tf',TFMssage,self.tf,queue_size=1)
+		self.sub_tf=rospy.Subscriber('/odom',Odometry,self.position,queue_size=1)
                 self.vel_x = rospy.get_param('~vel_x', 0.2)
         	self.vel_rot = rospy.get_param('~vel_rot',0.5)
 		self.pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
@@ -21,16 +23,16 @@ class Kobuki():
 	def feel(self,dockinfrared):
 		receivedDataArray = [ord(n) for n in dockinfrared.data]
 		vel = Twist()
-	        print(receivedDataArray)
+	        #print(receivedDataArray)
                 if receivedDataArray[0]==0 and receivedDataArray[1]==0 and receivedDataArray[2]==0:
         	#print(dockinfrared)
                     vel.angular.z=self.vel_rot
-            	    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+            	    #print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
                 self.pub.publish(vel)        	
 		#print("dockinfrared.data[0]=")
         	#print(dockinfrared.data[0])
         	#print("dockinfrared.data[1]=")i
-        def tf(self,tf):
+        def position(self,tf):
             print(tf)
 if __name__=="__main__":
     rospy.init_node('kobuki3')

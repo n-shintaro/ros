84d75a8c8ec3eb8a19520309c92bb5557e493e7e
n-shintaro <nakaoka.shintaro.infratop@gmail.com>
2019-05-10 20:12:19 +0900

    instance error

--- kobukiDockir.py
+++ kobukiDockir.py
@@ -5,13 +5,14 @@ from geometry_msgs.msg import Twist
 from kobuki_msgs.msg import BumperEvent
 from kobuki_msgs.msg import WheelDropEvent
 from kobuki_msgs.msg import DockInfraRed
+from nav_msgs.msg import Odometry
 
 class Kobuki():
     def __init__(self):
         self.vel_x = rospy.get_param('~vel_x', 0.2)
         self.vel_rot = rospy.get_param('~vel_rot',0.5)
         self.sub_dock_ir=rospy.Subscriber('/mobile_base/sensors/dock_ir',DockInfraRed,self.dock_ir_callback,queue_size=1)
-
+	self.sub_odometry=rospy.Subscriber('/odom',Odometry,self.position,queue_size=10)
         self.pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
         rospy.init_node("kobukiDockir")
         self.sub_bumper = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, self.bumper_callback,queue_size=1)
@@ -19,13 +20,30 @@ class Kobuki():
         self.bump=0
         self.wheel_drop_flag=0
         self.count=0
-        dock_ir_flag=0
+        self.dock_ir_flag=0
+        self.angle=0
+        self.angle_first_flag=0
+        self.angle_first=0
+    def position(self,odom):
+        self.angle=0
+        print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        if self.angle==odom.twist.twist.angular.z and self.angle_first_flag==0:
+            self.angle_first_flag=1
+            self.angle=odom.twist.twist.angular.z
+            self.angle_first=self.angle
+        elif not self.angle==odom.twist.twist.angular.z and self.angle_first_flag==1:
+            self.angle=odom.twist.twist.angular.z
     def controller(self):
 	        r = rospy.Rate(10.0)
                 ran=random.randrange(50, 100)
                 while not rospy.is_shutdown():
 		    vel = Twist()
-		    if self.bump==1 and self.wheel_drop_flag==0 and self.count<20:
+                    if self.bump==1:
+                                vel.linear.x=-0.1
+
+                    if self.dock_ir_flag==0 and self.bump==0 and self.wheel_drop_flag==0 and not self.angle-self.angle_first<350:
+                               vel.angular.z=self.vel_rot
+                    elif self.bump==1 and self.wheel_drop_flag==0 and self.count<20:
 		    		vel.linear.x=-0.1
 		    		self.count=self.count+1
 		    elif self.bump==1 and self.wheel_drop_flag==0 and self.count>=20 and self.count<ran:
@@ -44,6 +62,7 @@ class Kobuki():
 		    self.pub.publish(vel)
 		    r.sleep()
     #bumper ivent
+
     def bumper_callback(self,bumper):
         print("state"+str(bumper.state))
         self.bump=1
@@ -63,9 +82,6 @@ class Kobuki():
         #print(dockinfrared)
         #print("dockinfrared.data[0]=")
         #print(dockinfrared.data[0])
-            vel=Twist()
-            vel.angular.z=self.vel_rot
-            self.pub.publish(vel)
             dock_ir_flag=0
         #print("dockinfrared.data[1]=")
 if __name__=="__main__":
